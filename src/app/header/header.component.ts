e4e282edba23c98b17aa6e03106ead454575a5fa
goran.nedelkovski <gorannedelkovski87@yahoo.com>
2021-04-19 12:40:25 +0200

    [Section12 Course project-Routing, lectures157. Adding Navigation to the App, 158. Marking Active Routes,159. Fixing Page Reload Issues]-Comments=>[//1.(lecture.157-Adding navigation to the app)//remove (or comment) the event listeners on link and href and instead that, add routerLink directive="/recipes(with / is apsolute path, and without /is relative path)"--><!--1.(158.marking active routes)             //we need to dinamicaly add the bootstrap class="active" in the both li items->with directive routerLinkActive="active"--> //1(159-Fixing Page Reload Issues).//remove href attribubte(which sends request to the browser and reloads the page) and instead thgat add style="cursor:pointer"; //now without href, we don't reload the page any more--><!--2.(159)-//Remove href(to avoid reload the page t.e. to avoid sending http requests to the browser that will slow out app) also on these links in the dropdown and add attribute style="cursor:poniter" on the 2 links--><!--2.(159)-//Remove href(to avoid reload the page) also on these links in recipes-dertailad add attribute style="cursor:poniter" on the 3 links--> ]

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipes/recipes.model';
 
 @Component({
@@ -9,16 +9,16 @@ import { Recipe } from '../recipes/recipes.model';
 export class HeaderComponent implements OnInit {
   //3.create own event featureSelected
   //with @Output decorator, this our own event will be listenable from outside(from our parent component, app comp)
-  @Output() featureSelected = new EventEmitter<string>()
+  // @Output() featureSelected = new EventEmitter<string>()
 
   constructor() { }
 
   ngOnInit(): void {
   }
 //2.define that method, we expect to receaive as parameter some feature:string
-  onSelect(feature:string) {
-    this.featureSelected.emit(feature) //4.emit our feature:string that we received as parameter value 'recipe' string in our methods call in the template 
-  }
+  // onSelect(feature:string) {
+  //   this.featureSelected.emit(feature) //4.emit our feature:string that we received as parameter value 'recipe' string in our methods call in the template 
+  // }
   // onSelect('shopping-list') {
 
   // }
