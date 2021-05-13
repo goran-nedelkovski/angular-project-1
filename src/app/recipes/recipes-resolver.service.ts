1bdfff31355a675e50c41c992b91d413960d1349
goran.nedelkovski <gorannedelkovski87@yahoo.com>
2021-05-13 15:00:11 +0200

    [Section 19, c.project-Http, lecture284. Fixing a Bug with the Resolver]-Comments=>[ //1'(284)inject Recipes Service in the constructor and in the reslve() first check if we have some recipes, and only fetch new recipes from the server if we dont have existing recipes     const recipes = this.recipesService.getRecipes(); //get the existing recipes with getRecipes() from recipesService and store in variable recipes     if(recipes.length === 0) { //check if length of the recipes array is 0 (if recipes.length === 0)         return this.dataStorageServive.fetchRecipes(); //then fetch new recipes from the server     }     else {  //else if recipes.length > 0 (we have some existing recipe items in existing recipes array)         return recipes;  //then return this existing recipes array ]

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -7,6 +7,7 @@ import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/r
 import { Observable } from "rxjs";
 import { DataStorageService } from "../shared/data-storage.service";
 import { Recipe } from './recipes.model';
+import { RecipesService } from "./recipes.service";
 
 //2.(284)add @Inhjectable({providedIn:'root'}) decorator with the modern way {providedIn:'root'} instead of adding this Service in providers:[] in app.module.ts(import it from@ang/core)
 @Injectable({providedIn: 'root'})
@@ -15,14 +16,24 @@ export class RecipesResolverService implements Resolve<Recipe[]> {
 //3.(284)this service needs to implements Resolve interface<gereric interface, we must here define which type of data will be resolve in the end, in our case that data which we will resolve is Recipe[] (import Recipe from recipe.model )> (import it from @angular/router)
 //3.so our resolve code runs some code that loads some Recipes (Recipe[])
 //4.(284) inject our DataStorage Service here in the constructor(), becase that is the service that make the http request
-    constructor(private dataStorageServive: DataStorageService) { }
+    constructor(private dataStorageServive: DataStorageService,
+         private recipesService:RecipesService) { }
 
 //5.(284) Resolver interface forces us to add resolve(1st argument is the data about the current active route->route:ActivatedRouteSnapshot, 2nd argument is state:RouterStateSnapshot) method
     resolve(route: ActivatedRouteSnapshot, state:RouterStateSnapshot) {
     //5.we could get the information about the id that we request to, but here we want to load all recipes[]
     //5.the goal here is to return array of recipes(because we loaded first recipe[]) or an Observable that contain/emit that resolved array of Recipes.(go to data-stogare.service.ts)
 //9.(284)(here in the resolve() we return fetchRecipe() Observable from data Storage Service.I will not subscribe() here, but the resolve() will automaticaly subscibe for me to see/resolve once the data is there (t.e. resolver will subscribe for me to see/resolve if the recipes[] data is there)
-        return this.dataStorageServive.fetchRecipes();
+////////////////284. Fixing a Bug with the Resolver
+//1'(284)inject Recipes Service in the constructor and in the reslve() first check if we have some recipes, and only fetch new recipes from the server if we dont have existing recipes
+    const recipes = this.recipesService.getRecipes(); //get the existing recipes with getRecipes() from recipesService and store in variable recipes
+    if(recipes.length === 0) { //check if length of the recipes array is 0 (if recipes.length === 0)
+        return this.dataStorageServive.fetchRecipes(); //then fetch new recipes from the server
+    }
+    else {  //else if recipes.length > 0 (we have some existing recipe items in existing recipes array)
+        return recipes;  //then return this existing recipes array 
+    }
+    
 //9.(284)here we call fetchRecipe() Observable whenever this route is loaded (this is important to make sure that our data is realy there when we need it)
 //9.I will not subscribe() here, but the resolve() will automaticaly subscibe for me to see/resolve once the data is there (go to app-routing.module to apply this resolver)
     }
