b969749a5a39ad6392cd4f40974569dff0ebde8d
goran.nedelkovski <gorannedelkovski87@yahoo.com>
2021-05-18 15:13:13 +0200

    [Section 20, c.project-Auth..., lecture 296. Sending Login Requests]-Comments=>[ //1.(296)lets create login(receive email:string and password:string as parameters) in auth.service.we can see the signIn method with email and password how will look like in google->Firebase Auth Rest Api documentation; http.post('1st arguments is url..copy-paste the url(api endpoint and replace that [API_KEY] including the [] with our Web Api key in project_settings), 2nd argument is request body payload is with email, password and returnSecureToken(the same as before, copya and paste them in a js object {} as second argument); //2.(296)response Data Payload for login(signIn) is almost the same (plus registered property:boolean)..So, to be able to use the same Interface, I will add plus registered property with ? (optional, because signUp() will not use that, but login() will use this registered property)     registered?: boolean; //3.(296) in post<here define the type of the data that we get back from the api and that is AuthResponseData interface object>. finaly we need to 'return' this prepared Observable (to can subscribe() to our auth.component that is interested/care about the response); if(this.isLoginMode) {       // ... //4.(296)here in this.isLoginMode will be the login() logic.call the login(also forward the extracted email and password here as arguments) from Auth Service and subscribe() to it(subscribe to that observable that returns/give us back <AuthResponseData interface object>); //5.(296)we have duplicate code(in login and sighUp logic), so to avoid that, we can create a variable let authObs: Observable<generic type that returns/give us back that AuthResponseData(we need to export that interface to can use here, to can import it first at the top)> (import Observable from 'rxjs') let authObs: Observable<AuthResponseData>//5.this <AuthResponseData> will be used here by auth:Observable; //5-1.(296)the idea with this variable is to store both Observables(this.http.login() and this.http.signUp() to this authobs:Observable variable (without the subscribe code) and after if-else we can subscribe() to that authObs:Observable variable)       authObs = this.authService.login(email, password); authObs = this.authService.signUp(email, password);  //5-2.(296)the idea with this variable is to store both Observables(this.http.login() and this.http.signUp() to this authobs:Observable variable (without the subscribe code) and after if-else we can subscribe() to that authObs:Observable variable)     authObs.subscribe(         resData => { //4.we know that we will get/receive that AuthResponseData interface object, that we can store in some variable resData and simply console thatg varibale           console.log(resData);           this.isLoading = false;         }, error => {           console.log(error);           this.error = 'An Error occus';           this.isLoading = false; });//5'(239)now if we login with the same email and password, in the console we succefuly get back the Response object (..plus with property registered: true;//which means the user is registered in the db with signUp/register logic) ]

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { AuthResponseData } from './auth.service'; //import the interface AuthResponseData
 //////////////////286.How authentication works
 //1.(286)Client send auth data to the server.Server is Restful Api and it dont know the client(not care about client).Client-server communicate through http:HttpClient req/res. So 1st we check on the client side validation of the credentials(validation of username and password), and this auth data we send to the server.The server also check if the username and password are valid and if the are valied, the server create Token (with secret algoritams) and send that token to the client.That Token is encoded JSON String(not encripted).Client stores that Token in the Stogare(In the Browser's Local Storage).And this token will be attached on any request(attached on the header like query params itn..) send to the server and these request must be authenticated/authorize.
 //////////////////287. Adding the Auth Page
@@ -39,29 +41,58 @@ export class AuthComponent implements OnInit {
   //1.(293)here in onSubmit before form.reset, first I want to extract my email and password , t.e. to get the values of the email and password and store in variabvles(with form.value.email and form.value.password)
     const email = form.value.email;
     const password = form.value.password;
+    //5.(296)we have duplicate code(in login and sighUp logic), so to avoid that, we can create a variable let authObs: Observable<generic type that returns/give us back that AuthResponseData(we need to export that interface to can use here, to can import it first at the top)> (import Observable from 'rxjs')
+    //5.(296)the idea with this variable is to store both Observables(this.http.login() and this.http.signUp() to this authobs:Observable variable (without the subscribe code) and after if-else we can subscribe() to that authObs:Observable variable)
+    let authObs: Observable<AuthResponseData>//5.this <AuthResponseData> will be used here by auth:Observable
   //4.(294)add here in onSubmit() right befor sendich the form, re-set isLoading to true.
     this.isLoading = true;
   //4.(293) check if we are in isLoginMode then here will be SignIn() logic (here we can't do nothing for now, because we did't add singIn logic yet, we only added signUp logic) and else (if we are not on isLoginMode then, here will be signUp logic)
     if(this.isLoginMode) {
-      // ... //here will be the signIn() logic
+      // ... //4.(296)here in this.isLoginMode will be the login() logic.call the login(also forward the extracted email and password here as arguments) from Auth Service and subscribe() to it(subscribe to that observable that returns/give us back <AuthResponseData interface object>)
+     //5-1.(296)the idea with this variable is to store both Observables(this.http.login() and this.http.signUp() to this authobs:Observable variable (without the subscribe code) and after if-else we can subscribe() to that authObs:Observable variable)
+      authObs = this.authService.login(email, password);
+      // this.authService.login(email, password).subscribe(
+      //   resData => { //4.we know that we will get/receive that AuthResponseData interface object, that we can store in some variable resData and simply console thatg varibale
+      //     console.log(resData);
+      //     this.isLoading = false;
+      //   }, error => {
+      //     console.log(error);
+      //     this.error = 'An Error occus';
+      //     this.isLoading = false;
+      //   }
+      // );
     }
-    else { //here will be the singUp logic
+    else { //here will be the singUp/register logic
+  //5-1.(296)the idea with this variable is to store both Observables(this.http.login() and this.http.signUp() to this authobs:Observable variable (without the subscribe code) and after if-else we can subscribe() to that authObs:Observable variable)
+      authObs = this.authService.signUp(email, password);
       //3.(293)in else (if we are not on this.isLoginMode), then here will be the signUp () logic
       //3.(293)we need to inject our auth Service here in our auth component, so we can call signUp(with extracted email and password as parameters) method from the Service and subscribe() to this singUp() method (to this Observable)
-    this.authService.signUp(email, password).subscribe(
-      resData => {//we expect to receive resData:AuthResponseData interface(response data payload, as success response)
-        console.log(resData)
-      //5.(294)here as soon as we are done with sending the form(t.e. when we get the response), set back isLoading to false (in both case, in the success case and in the error case)
-        this.isLoading = false;
-      }, error => { //2nd argument in subscribe will be error callback (if we get an error obj)
-        console.log(error)
-      //8.(294)also I want to show error message if we get an arror here t.e. if we have a problem with login.for that I will create error property to store the error:string messagge and initialy set to null and here we get the error.message and store to that error:string property (go to the template)
-        this.error = 'An error occur';
-        //5.(294)here as soon as we are done with sending the form(t.e. when we get the response), set back isLoading to false (in both case, in the success case and in the error case)
-        this.isLoading = false;
-    //6.(294)now isLoading property we can use in the template to hide the form
-      });
+    // this.authService.signUp(email, password).subscribe(
+    //   resData => {//we expect to receive resData:AuthResponseData interface(response data payload, as success response)
+    //     console.log(resData)
+    //   //5.(294)here as soon as we are done with sending the form(t.e. when we get the response), set back isLoading to false (in both case, in the success case and in the error case)
+    //     this.isLoading = false;
+    //   }, error => { //2nd argument in subscribe will be error callback (if we get an error obj)
+    //     console.log(error)
+    //   //8.(294)also I want to show error message if we get an arror here t.e. if we have a problem with login.for that I will create error property to store the error:string messagge and initialy set to null and here we get the error.message and store to that error:string property (go to the template)
+    //     this.error = 'An error occur';
+    //     //5.(294)here as soon as we are done with sending the form(t.e. when we get the response), set back isLoading to false (in both case, in the success case and in the error case)
+    //     this.isLoading = false;
+    // //6.(294)now isLoading property we can use in the template to hide the form
+    //   });
     }
+  //5-2.(296)the idea with this variable is to store both Observables(this.http.login() and this.http.signUp() to this authobs:Observable variable (without the subscribe code) and after if-else we can subscribe() to that authObs:Observable variable)
+    authObs.subscribe(
+        resData => { //4.we know that we will get/receive that AuthResponseData interface object, that we can store in some variable resData and simply console thatg varibale
+          console.log(resData);
+          this.isLoading = false;
+        }, error => {
+          console.log(error);
+          this.error = 'An Error occus';
+          this.isLoading = false;
+        }
+    );
+//5'(296)now if we login with the same email and password, in the console we succefuly get back the Response object (..plus with property registered: true;//which means the user is registered in the db with signUp/register logic)
   //5.(239)we can see in the console(if we signUp, switch to signUp) the new registered User object and we can se that registered User in firebase.com -> Authenticate -> Users (Refresh)).But if we input the exactly email and password (or at least the same email) we get an error because thatg User already exists
     form.reset();//after sending/sumbiting the form, reset the form with form.reset() like at the begining with empty fields.
   }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,19 +6,21 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 //9.(292)lets create a new interface AuthResponseData(no need to export) for the response data that we get back from the api t.e. <the Response data that we get back/retrieve with the post<AuthResponseData>()>(see the Response payload from the documentation)(good practice is to create an interface, which is very helpful when we work with the response data)
-interface AuthResponseData {
+ export interface AuthResponseData {
     idToken: string, 
     email: string, 
     refreshToken: string, 
     expiresIn: string,
-    localId: string
+    localId: string,
+    //2.(296)response Data Payload for login(signIn) is almost the same (plus registered property:boolean)..So, to be able to use the same Interface, I will add plus registered property with ? (optional, because signUp() will not use that, but login() will use this registered property) 
+    registered?: boolean
 }
 //3.(292)export this class Auth Service and add @Injectable({providedIn:'roto'}0import it from @ang/core)
 @Injectable({providedIn:'root'}) //if we inject service into a service like here, we need to add @Injectable()
 export class AuthService {
 //5.(292)we need http:HttpClient to send that requests.inject it in the constructor
     constructor(private http:HttpClient) {}
-//4.(292)add signUp() method.This method should send our request to that signUp url(to signUp endpoint)
+//4.(292)add signUp()/register method.This method should send our request to that signUp url(to signUp endpoint)
 //4.(292) signUp(expect to receive as parameters/input values email:string, password:string) 
     signUp(email:string, password:string) { //4.expect to receive as parameters/value email:string, password:string
 //6.(292)here in signUp send http.post() request to that signUp url with {request body payload data: email, password, returnSercureToken:true}
@@ -34,4 +36,16 @@ export class AuthService {
             returnSecureToken: true //also Api expect returnSecureToken and set to true(always, as shown in the documentation)
         });
     }
+///////////////////296. Sending Login Requests
+//1.(296)lets create login(receive email:string and password:string as parameters) in auth.service.we can see the signIn method with email and password how will look like in google->Firebase Auth Rest Api documentation; http.post('1st arguments is url..copy-paste the url(api endpoint and replace that [API_KEY] including the [] with our Web Api key in project_settings), 2nd argument is request body payload is with email, password and returnSecureToken(the same as before, copya and paste them in a js object {} as second argument)
+    login(email: string, password: string) {
+    //3.(296) in post<here define the type of the data that we get back from the api and that is AuthResponseData interface object>. finaly we need to 'return' this prepared Observable (to can subscribe() to our auth.component that is interested/care about the response) 
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBKqQxisoIf6sBjIh0JV7xO126_N7tE4JU', 
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        });
+    }
+
 }
